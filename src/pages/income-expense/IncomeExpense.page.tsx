38f50fb34590fa50a841e47fdd0441de987b0dab
abek01sulaymonov <abek01sulaymonov@gmail.com>
2025-04-07 02:09:04 +0500

    fix

--- src/pages/income-expense/IncomeExpense.page.tsx
+++ src/pages/income-expense/IncomeExpense.page.tsx
@@ -24,7 +24,7 @@ import TopbarLayout from '@/components/layout/dashboard/Topbar.layout';
 const IncomeExpensePage = () => {
   return (
     <>
-      <Stack p="16px 8px 16px 8px" h="100%" w='calc(100vw - 250px - 72px)'>
+      <Stack p="16px 8px 16px 8px" h="100%">
         <TopbarLayout title="Income & Expense" />
         <Stack
           component="main"
